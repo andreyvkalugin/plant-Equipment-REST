bf34c6d725c4feb9dfb74cc98829f37bbd0baa61
andreyvkalugin <42234223aA>
2020-08-05 23:11:42 +0300

    freemarker use

--- src/main/java/ru/plantEquipment/controller/MainController.java
+++ src/main/java/ru/plantEquipment/controller/MainController.java
@@ -1,20 +1,28 @@
 package ru.plantEquipment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import ru.plantEquipment.domain.Message;
 import ru.plantEquipment.domain.User;
 import ru.plantEquipment.repos.MessageRepo;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
+    @Value("${upload.path}")
+    private String uploadPath;
+
     @Autowired
     private MessageRepo messageRepo;
 
@@ -43,9 +51,25 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model
-    ) {
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+            int len = file.getOriginalFilename().length();
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + file.getOriginalFilename().substring(len-4, len);
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
 
         messageRepo.save(message);
 
